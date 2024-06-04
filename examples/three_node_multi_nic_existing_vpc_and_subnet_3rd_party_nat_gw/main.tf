d222cf10452cb52d22af786a013be991276377c6
c.klewar <c.klewar@f5.com>
2024-06-04 18:20:08 +0200

    remove unneeded zone

--- examples/three_node_multi_nic_existing_vpc_and_subnet_3rd_party_nat_gw/main.tf
+++ examples/three_node_multi_nic_existing_vpc_and_subnet_3rd_party_nat_gw/main.tf
@@ -6,60 +6,7 @@ locals {
   }
 }
 
-module "vpc_slo" {
-  source       = "terraform-google-modules/network/google"
-  mtu          = 1460
-  version      = "~> 6.0"
-  project_id   = var.gcp_project_id
-  network_name = "${var.project_prefix}-${var.project_name}-vpc-slo-${var.gcp_region}-${var.project_suffix}"
-  subnets = [
-    {
-      subnet_name   = "${var.project_prefix}-${var.project_name}-slo-${var.gcp_region}-${var.project_suffix}"
-      subnet_ip     = "192.168.1.0/24"
-      subnet_region = var.gcp_region
-    }
-  ]
-}
-
-module "vpc_sli" {
-  source       = "terraform-google-modules/network/google"
-  version      = "~> 6.0"
-  project_id   = var.gcp_project_id
-  network_name = "${var.project_prefix}-${var.project_name}-vpc-sli-${var.gcp_region}-${var.project_suffix}"
-  mtu          = 1460
-  subnets = [
-    {
-      subnet_name   = "${var.project_prefix}-${var.project_name}-sli-${var.gcp_region}-${var.project_suffix}"
-      subnet_ip     = "192.168.2.0/24"
-      subnet_region = var.gcp_region
-    }
-  ]
-  delete_default_internet_gateway_routes = true
-}
-
-resource "google_compute_address" "nat" {
-  count   = 1
-  name    = "${module.vpc_slo.network_name}-${var.gcp_region}-nat-${count.index}"
-  project = var.gcp_project_id
-  region  = var.gcp_region
-}
-
-module "nat" {
-  source        = "terraform-google-modules/cloud-nat/google"
-  version       = "~> 2.0"
-  project_id    = var.gcp_project_id
-  region        = var.gcp_region
-  router        = "${var.project_prefix}-${var.project_name}-nat-router-${var.gcp_region}-${var.project_suffix}"
-  create_router = true
-  name          = "${var.project_prefix}-${var.project_name}-nat-config-${var.gcp_region}-${var.project_suffix}"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-  # nat_ip_allocate_option             = "MANUAL_ONLY"
-  nat_ips       = google_compute_address.nat.*.self_link
-  network       = module.vpc_slo.network_name
-}
-
 module "f5xc_gcp_cloud_ce_three_node_multi_nic_existing_vpc_and_subnet_3rd_party_nat_gw" {
-  depends_on                      = [module.vpc_slo, module.vpc_sli, module.nat]
   source                          = "../../modules/f5xc/ce/gcp"
   owner                           = var.owner
   is_sensitive                    = false
